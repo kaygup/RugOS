29296b94ed37fa248aacd217bffab252fc3d605d
Kay <uhbonaparte@gmail.com>
2025-04-19 01:08:53 +0000

    Update kernel.c

--- OS/kernel.c
+++ OS/kernel.c
@@ -1,5 +1,26 @@
+#include "include/kb.h"
 #include "include/screen.h"
 int kmain()
 {   
-    
+    clearScreen();
+    print("Rug OS 1.1\nrugshell 1.1");
+    while (1)
+    {
+        print("\nRUGOS> ");
+
+        string ch = readStr();
+        if(strEql(ch, "version"))
+        {
+            print("\nRugOS 1.1\n");
+        }
+        else if(strEql(ch, "clear")) 
+        {
+            clearScreen();
+        }
+        else 
+        {
+            print(ch);
+            print(" is not a command!\n");
+        }
+    }
 }
