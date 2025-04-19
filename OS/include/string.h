c632015f976c009ad401b030edf833f350181197
Kay <uhbonaparte@gmail.com>
2025-04-19 01:09:11 +0000

    Update string.h

--- OS/include/string.h
+++ OS/include/string.h
@@ -10,4 +10,20 @@ uint16 strlenght(string ch)
     return --i;
 }
 
+uint8 strEql(string ch1, string ch2)
+{
+    uint8 result = 1;
+    uint8 size = strlenght(ch1);
+    if(size != strlenght(ch2)) result = 0;
+    else
+    {
+        uint8 i = 0;
+        for(i;i<=size;i++)
+        {
+            if(ch1[i] != ch2[i]) result = 0;
+        }
+    }
+    return result;
+}
+
 #endif
